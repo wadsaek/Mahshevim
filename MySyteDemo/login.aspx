4fa99e80fe4bfb93686006def97bad25453db83b
wadsaek <141351086+wadsaek@users.noreply.github.com>
2024-01-08 19:06:34 +0200

    Add files via upload

---
+++ MySyteDemo/login.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" action="login.aspx" method="post">
+        <div>
+            <input type="text" id="userName" name="userName" placeholder="User name" />
+            <input type="password" id="password" name="password" placeholder="Password" />
+            <input type="submit" value="Login" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MySyteDemo/login.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+
+    //string path = 
+    static SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\yoky\mySite\App_Data\myDb.mdf;");
+    static SqlCommand cmd = new SqlCommand("", connection);
+    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Form.Count > 0)
+        {
+
+            cmd.CommandText = String.Format("select * from users where userName='{0}' and password='{1}';",
+                Request.Form["userName"].ToString(), Request.Form["password"].ToString());
+            
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                Response.Write("Hello" + dataTable.Rows[0]["firstName"] + " " + dataTable.Rows[0]["lastName"]);
+            }
+            else
+            {
+                Response.Write("Wrong Details");
+            }
+        }
+    }
+}
