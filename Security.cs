cb1b60852a3702e2aa884312d384281c9f0a7cc8
wadsaek <141351086+wadsaek@users.noreply.github.com>
2024-01-18 10:05:50 +0200

    security algorithms
    random salt generator 
    hash256 with salt

---
+++ Security.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// Summary description for Security
+/// </summary>
+public class Security
+{
+    static string RandomString(int length)
+    {
+        const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}[]`~ -_=+|';:<,>.?/";
+        StringBuilder res = new StringBuilder();
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] uintBuffer = new byte[sizeof(uint)];
+
+            while (length-- > 0)
+            {
+                rng.GetBytes(uintBuffer);
+                uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                res.Append(valid[(int)(num % (uint)valid.Length)]);
+            }
+        }
+
+        return res.ToString();
+    }
+
+    public static string EncryptPassword(string password, string salt)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var saltedPassword = string.Format("{0}{1}", salt, password);
+            byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
+            return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordAsBytes));
+        }
+    }
+
+
+}
